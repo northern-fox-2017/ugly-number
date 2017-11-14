8c91bd7a1eb551a2815edcf16195f7dd2d2a3102
Haniiful Wahib <wahib@Haniifuls-MacBook-Pro.local>
2017-11-14 11:16:55 +0700

    initial submit by Wahib

--- uglyNumber.js
+++ uglyNumber.js
@@ -1,31 +1,32 @@
 'use strict'
 
 function maxDivides(number, divisible) {
-  while(number % divisible == 0) {
+  while(number % divisible === 0) {
     number = number / divisible;
   }
-  return numbers;
+  return number;
 }
 
 function isUgly(numberUgly) {
-  numberUgly = maxDivide(numberUgly, 2);
-  numberUgly = maxDivide(numberUgly, 3);
-  numberUgly = maxDivide(numberUgly, 5);
+  numberUgly = maxDivides(numberUgly, 2);
+  numberUgly = maxDivides(numberUgly, 3);
+  numberUgly = maxDivides(numberUgly, 5);
 
-  return (numbers = 1) ? true : false;
+  return (numberUgly === 1) ? true : false;
 }
 
 function getUglyNo(input) {
   let i = 1;
-  let countUglyNumber == 1; // ugly number count
+  let countUglyNumber = 1; // ugly number count
 
   while(input > countUglyNumber) {
+    //debugger
     i++;
-    if(isUgly(i) == 1) {
-      counts++;
+    if (isUgly(i) === true) {
+      countUglyNumber++;
     }
-    return i;
   }
+  return i;
 }
 
 //driver code
@@ -33,3 +34,5 @@ console.log(getUglyNo(150)); //5832
 console.log(getUglyNo(7)); //8
 console.log(getUglyNo(10)); //12
 console.log(getUglyNo(15)); //24
+
+//1 2 3 4 5 6 8 9 10 12 15 
