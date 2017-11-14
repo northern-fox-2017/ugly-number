b7e39511c266b7144afb5bd55440b2b3d88bb1af
idabagusangga <angga.idabagus@gmail.com>
2017-11-14 10:41:59 +0700

    bug SQUASHED!

--- uglyNumber.js
+++ uglyNumber.js
@@ -1,10 +1,10 @@
 'use strict'
 
-function maxDivides(number, divisible) {
+function maxDivide(number, divisible) {
   while(number % divisible == 0) {
     number = number / divisible;
   }
-  return numbers;
+  return number;
 }
 
 function isUgly(numberUgly) {
@@ -12,20 +12,21 @@ function isUgly(numberUgly) {
   numberUgly = maxDivide(numberUgly, 3);
   numberUgly = maxDivide(numberUgly, 5);
 
-  return (numbers = 1) ? true : false;
+  return (numberUgly = 1) ? true : false;
 }
 
 function getUglyNo(input) {
   let i = 1;
-  let countUglyNumber == 1; // ugly number count
+  let countUglyNumber = 1; // ugly number count
 
   while(input > countUglyNumber) {
     i++;
     if(isUgly(i) == 1) {
-      counts++;
+      countUglyNumber++;
     }
-    return i;
+
   }
+  return i;
 }
 
 //driver code
