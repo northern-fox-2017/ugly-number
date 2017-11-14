aff54cbca678a9e9dccac0c4371cdcb5b165da2f
yofri <yofriadiyahya@gmail.com>
2017-11-14 11:11:01 +0700

    Finished uglyNumber

--- uglyNumber.js
+++ uglyNumber.js
@@ -1,35 +1,37 @@
 'use strict'
 
 function maxDivides(number, divisible) {
-  while(number % divisible == 0) {
+  while (number % divisible === 0) {
     number = number / divisible;
   }
-  return numbers;
+  return number;
 }
 
 function isUgly(numberUgly) {
-  numberUgly = maxDivide(numberUgly, 2);
-  numberUgly = maxDivide(numberUgly, 3);
-  numberUgly = maxDivide(numberUgly, 5);
+  numberUgly = maxDivides(numberUgly, 2);
+  numberUgly = maxDivides(numberUgly, 3);
+  numberUgly = maxDivides(numberUgly, 5);
 
-  return (numbers = 1) ? true : false;
+  return numberUgly === 1 ? true : false;
 }
 
 function getUglyNo(input) {
   let i = 1;
-  let countUglyNumber == 1; // ugly number count
+  let countUglyNumber = 1; // ugly number count
 
-  while(input > countUglyNumber) {
+  while (input > countUglyNumber) {
     i++;
-    if(isUgly(i) == 1) {
-      counts++;
+
+    if (isUgly(i) == 1) {
+      countUglyNumber++;
     }
-    return i;
   }
+
+  return i;
 }
 
-//driver code
-console.log(getUglyNo(150)); //5832
-console.log(getUglyNo(7)); //8
-console.log(getUglyNo(10)); //12
-console.log(getUglyNo(15)); //24
+// driver code
+console.log(getUglyNo(150)); // 5832
+console.log(getUglyNo(7)); // 8
+console.log(getUglyNo(10)); // 12
+console.log(getUglyNo(15)); // 24
