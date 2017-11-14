029e02b1bcbad89a799cdc3b20c758bac903b835
juliusade <juliusade15@gmail.com>
2017-11-14 11:54:01 +0700

    release0:debug

--- uglyNumber.js
+++ uglyNumber.js
@@ -4,28 +4,28 @@ function maxDivides(number, divisible) {
   while(number % divisible == 0) {
     number = number / divisible;
   }
-  return numbers;
+  return number;
 }
-
+// debugger
 function isUgly(numberUgly) {
-  numberUgly = maxDivide(numberUgly, 2);
-  numberUgly = maxDivide(numberUgly, 3);
-  numberUgly = maxDivide(numberUgly, 5);
+  numberUgly = maxDivides(numberUgly, 2);
+  numberUgly = maxDivides(numberUgly, 3);
+  numberUgly = maxDivides(numberUgly, 5);
 
-  return (numbers = 1) ? true : false;
+  return (numberUgly == 1) ? true : false;
 }
 
 function getUglyNo(input) {
   let i = 1;
-  let countUglyNumber == 1; // ugly number count
-
+  let countUglyNumber = 1; // ugly number count
+  // debugger
   while(input > countUglyNumber) {
     i++;
     if(isUgly(i) == 1) {
-      counts++;
+      countUglyNumber++;
     }
-    return i;
   }
+  return i;
 }
 
 //driver code
@@ -33,3 +33,5 @@ console.log(getUglyNo(150)); //5832
 console.log(getUglyNo(7)); //8
 console.log(getUglyNo(10)); //12
 console.log(getUglyNo(15)); //24
+
+// deret 1,2,3,4,5,6,8
