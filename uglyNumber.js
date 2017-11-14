60f55353f729b64819f655884e30b37d79f8f25f
Fajar Patappari <top.sick.red@gmail.com>
2017-11-14 11:22:50 +0700

    Ugly numbers

--- uglyNumber.js
+++ uglyNumber.js
@@ -4,31 +4,31 @@ function maxDivides(number, divisible) {
   while(number % divisible == 0) {
     number = number / divisible;
   }
-  return numbers;
+  return number;
 }
 
 function isUgly(numberUgly) {
-  numberUgly = maxDivide(numberUgly, 2);
-  numberUgly = maxDivide(numberUgly, 3);
-  numberUgly = maxDivide(numberUgly, 5);
+  numberUgly = maxDivides(numberUgly, 2);
+  numberUgly = maxDivides(numberUgly, 3);
+  numberUgly = maxDivides(numberUgly, 5);
 
-  return (numbers = 1) ? true : false;
+  return numberUgly === 1
 }
 
 function getUglyNo(input) {
   let i = 1;
-  let countUglyNumber == 1; // ugly number count
+  let countUglyNumber = 1; // ugly number count
 
   while(input > countUglyNumber) {
     i++;
-    if(isUgly(i) == 1) {
-      counts++;
+    if(isUgly(i)) {
+      countUglyNumber++;
     }
-    return i;
+      
   }
+  return i;
 }
 
-//driver code
 console.log(getUglyNo(150)); //5832
 console.log(getUglyNo(7)); //8
 console.log(getUglyNo(10)); //12
